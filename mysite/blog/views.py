565104718644605160ec8a31812cab99bf1c4fd1
Rufinad <alsergrits@gmail.com>
2023-05-16 23:02:22 +0300

    Blog site

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,18 +1,91 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+class PostlistView(ListView):
+    '''Альтернативное представление списка постов'''
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request,
-                 'blog/post/list.html',
-                 {'posts': posts})
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#
+#     # постраничная разбивка с 3 постами на страницу
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#
+#     try :
+#         posts = paginator.page(page_number)
+#
+#     except EmptyPage:  #  Если page_number находится вне диапазона то выдать послежнюю страницу
+#         posts = paginator.page(paginator.num_pages)
+#
+#     except PageNotAnInteger:  # Если page_number не int то выдай 1ю стр
+#         posts = paginator.page(1)
+#
+#
+#     return render(request,
+#                  'blog/post/list.html',
+#                  {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,
+                             )
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):  # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':  # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():  # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read" \
+            f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+            f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject,message, 'sangrits88@gmail.com', [cd['to']])
+            sent = True
+    # ... отправить электронное письмо
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form, 'sent': sent})
+
+
+@require_POST  # этот декаратор требует чтобы метод был только POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status = Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.Post)  # извлекаем из запроса PoST  его данные (имя, текст, почта)
+    if form.is_valid():  #  Создать объект класса Comment, не сохраняя его в базе данных
+        comment = form.save(commit=False)
+        # Назначить пост комментарию
+        comment.post = post
+        # Сохранить комментарий в бд
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
