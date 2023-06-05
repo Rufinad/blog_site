e8d8e1001ef197996f1ca1215e83032d26f4a852
Rufinad <alsergrits@gmail.com>
2023-06-05 22:18:44 +0300

    forms views template urls for searching

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,10 +3,11 @@ from taggit.models import Tag
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 class PostlistView(ListView):
     '''Альтернативное представление списка постов'''
@@ -112,3 +113,23 @@ def post_comment(request, post_id):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        results = Post.published.annotate(
+            search=SearchVector('title', 'body')
+        ).filter(search=query)
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
